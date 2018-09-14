2db56d882e1b5b5202397547e0768cd92d34484e
Sitraka Ratsimba <ratsimbasitraka@gmail.com>
2018-09-14 16:40:17 +0300

    validates Pcop::SubAccount

--- app/models/pcop/sub_account.rb
+++ app/models/pcop/sub_account.rb
@@ -1,3 +1,5 @@
 class Pcop::SubAccount < ApplicationRecord
   belongs_to :pcop_account, foreign_key: 'pcop_account_id', optional: true
+  validates  :id, format: { with: /\A[1-7][\d]{2}\z/ }
+  validates  :name, uniqueness: true
 end
--- test/models/pcop/sub_account_test.rb
+++ test/models/pcop/sub_account_test.rb
@@ -1,5 +1,26 @@
 require 'test_helper'
 
 class Pcop::SubAccountTest < ActiveSupport::TestCase
+  def setup
+    @sub_account = pcop_sub_accounts(:one)
+  end
 
+  test "is valid" do
+    assert @sub_account.valid?
+  end
+
+  test "id should contain 3 characters" do
+    @sub_account.id = 12
+    @sub_account.name = "fa"
+    assert_not @sub_account.valid?
+    @sub_account.id = 123
+    assert @sub_account.valid?
+  end
+
+  test "name should be unique" do
+    @sub_account.save
+    other_sub_account = pcop_sub_accounts(:two)
+    other_sub_account.name = @sub_account.name
+    assert other_sub_account.invalid?
+  end
 end

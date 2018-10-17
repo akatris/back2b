900460e6124a3d431d7f0e1fb5be886ebe1e40bc
Sitraka Ratsimba <ratsimbasitraka@gmail.com>
2018-10-17 15:49:43 +0300

    Update season table

--- app/javascript/components/budget_item/BudgetItemDetail.jsx
+++ app/javascript/components/budget_item/BudgetItemDetail.jsx
@@ -2,10 +2,47 @@ import React from 'react';
 import Table from './Table';
 
 export default class BudgetItemDetail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('/pcops.json')
+      .then(response => response.json())
+      .then(data => this.setState({data}))
+      .catch(error => console.log(error));
+  }
+
+  flatternData() {
+    let pcop_rubrics = [];
+
+    const getRubric = ({id, name, description, eligible_transactions}) => {
+      return {id, name, description, eligible_transactions}
+    };
+
+    for (const category of this.state.data) {
+      pcop_rubrics = pcop_rubrics.concat(getRubric(category));
+      for (const account of category.accounts) {
+        pcop_rubrics = pcop_rubrics.concat(getRubric(account));
+        for (const sub_account of account.sub_accounts) {
+          pcop_rubrics = pcop_rubrics.concat(getRubric(sub_account));
+          for (const rubric of sub_account.rubrics) {
+            pcop_rubrics = pcop_rubrics.concat(getRubric(rubric));
+          }
+        }
+      }
+    }
+    return pcop_rubrics;
+  }
+
   render() {
+
     return (
       <div>
-        <Table />
+        <Table rubrics={this.flatternData()} />
       </div>
     );
   }
--- app/javascript/components/budget_item/Row.jsx
+++ app/javascript/components/budget_item/Row.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
 export default class Row extends React.Component {
+  getClass(column) {
+    const classes = ['class', 'account', 'sub-account', 'rubric'];
+    const stringId = new String(this.props.rubric.id);
+    return `${classes[stringId.length - 1]}-${column}`;
+  }
+
   render() {
     return (
       <tr>
-        <td>{this.props.rubric.id}</td>
-        <td>{this.props.rubric.name}</td>
+        <th className={this.getClass('id')} scope="row">
+          <span>{this.props.rubric.id}</span>
+        </th>
+        <td className={this.getClass('name')}>{this.props.rubric.name}</td>
         <td>{this.props.rubric.description}</td>
         <td>{this.props.rubric.amount}</td>
       </tr>
